0e8592285cf7bd1141086b37e1f0879d7866ad16
Stanislav001 <bezqk14@abv.bg>
2021-12-08 08:32:02 +0200

    Add About View and edit Index View

--- AcademyProject/Controllers/HomeController.cs
+++ AcademyProject/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace AcademyProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
