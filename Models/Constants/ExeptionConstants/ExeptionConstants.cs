45d24c120893888371acca86715d33a9c58056bb
Stanislav001 <bezqk14@abv.bg>
2022-02-12 09:52:14 +0200

    Update

--- Models/Constants/ExeptionConstants/ExeptionConstants.cs
+++ Models/Constants/ExeptionConstants/ExeptionConstants.cs
@@ -2,7 +2,7 @@
 {
     public class ExeptionConstants
     {
-        public const string NOT_EXISTING_COURSE_ERROR_MESSAGE = "Such a course does not exist!";
         public const string NOT_EXISTING_USER_ERROR_MESSAGE = "Such a user does not exist!";
+        public const string NOT_EXISTING_COURSE_ERROR_MESSAGE = "Such a course does not exist!";
     }
 }
